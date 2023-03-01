b4b446df0c8010f6416e7e4b15e6c2a4f46058cc
TPSooryaRam <122258194+TPSooryaRam@users.noreply.github.com>
2023-03-01 18:04:45 +0530

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -4,5 +4,5 @@
           echo 2. Is Java Installed?
           echo java --${1}
           echo 3. Provide the Message:
-          echo "SUCCESSMESSAGE is equal to: ${{ SUCCESSMESSAGE }}"
-          echo "FAILUREMESSAGE is equal to: ${{ FAILUREMESSAGE }}"
+          echo "SUCCESSMESSAGE is equal to: ${{ vars.SUCCESSMESSAGE }}"
+          echo "FAILUREMESSAGE is equal to: ${{ vars.FAILUREMESSAGE }}"
