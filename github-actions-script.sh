ea36a5e36e51e8e86e5022cab6ad5178474a6556
TPSooryaRam <122258194+TPSooryaRam@users.noreply.github.com>
2023-03-01 16:24:32 +0530

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -3,9 +3,7 @@
           echo
           echo 2. Is Java Installed?
           echo java --${1}
-          echo 3. Who is running this script?
-          $(HOWAREYOU)
-          echo 4. How is the disc laid out?
-          df $HUMANEYES
-          echo 5. What environment variables are available?
-          env
+          echo 3. Provide the Message:
+          ${SUCCESSMESSAGE}
+          ${FAILUREMESSAGE}
+      
